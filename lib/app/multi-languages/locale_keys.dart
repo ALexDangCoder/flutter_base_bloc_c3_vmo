499dff944e06c1286ab4f4e73dbc03bc339d485d
ductungCoder <94129871+ductungCoder@users.noreply.github.com>
2023-05-31 15:52:46 +0700

    tung dx
    push test

--- lib/app/multi-languages/locale_keys.dart
+++ lib/app/multi-languages/locale_keys.dart
@@ -10,7 +10,7 @@ part 'locale_keys.g.dart';
 
 @SheetLocalization(
   docId: '1SpiJWFRfJaIRnzpEc0mJ2WaaI9JYlz8jKBPduAPzdXE',
-  version: 2,
+  version: 1,
   //You must increment it each time you want to regenerate a new version of the labels.
 )
 class _LocaleKeys {}
